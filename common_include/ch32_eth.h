f46232455fd0c402e8c773ccc79640248599a641
TOKITA Hiroshi <tokita.hiroshi@gmail.com>
2022-10-02 06:46:12 +0900

    common_include: add common API header
    Add common API header for abstract the SoC series names.
    
    Signed-off-by: TOKITA Hiroshi <tokita.hiroshi@gmail.com>

---
+++ common_include/ch32_eth.h
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) 2022 TOKITA Hiroshi <tokita.hiroshi@gmail.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#if defined(CONFIG_SOC_SERIES_CH32V30X)
+#include <ch32v30x_eth.h>
+#endif
